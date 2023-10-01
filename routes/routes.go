202548845fed260916d250bf69c4a0d66e579799
Ryan Schultz <rdschultz@verisk.com>
2023-10-01 11:24:00 -0600

    add auth middleware and JWT packages

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+	"sarkaz_api/auth"
 	"sarkaz_api/database"
 	"sarkaz_api/handlers"
 
@@ -10,6 +12,10 @@ import (
 func InitializeRoutes(r *gin.Engine, characterRepo *database.CharacterRepository) {
     api := r.Group("/api")
     {
+
+        api.GET("/protected", auth.AuthMiddleware(), func(c *gin.Context) {
+            c.JSON(http.StatusOK, gin.H{"message": "This is a protected route"})
+        })
         api.GET("/characters/:name", handlers.GetCharacterHandler(characterRepo))
         api.GET("/characters//simple/:name", handlers.GetSimpleCharacterHandler(characterRepo))
         api.GET("/characters/rarity/:stars", handlers.GetCharactersByRarityHandler(characterRepo))
