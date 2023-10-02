3138516eced13c8212cb1d0b93fcb1cc265d563b
Ryan Schultz <rdschultz@verisk.com>
2023-10-02 15:50:39 -0600

    enable requests from localhost dev environment

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"sarkaz_api/database"
 	"sarkaz_api/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +15,12 @@ func main() {
     characterRepo := database.NewCharacterRepository(database.DB)
 
     r := gin.Default()
+
+    config := cors.DefaultConfig()
+    config.AllowOrigins = []string{"http://localhost:4200"}
+    config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+
+    r.Use(cors.New(config))
 
     routes.InitializeRoutes(r, characterRepo)
 
