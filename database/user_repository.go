c5e1e8cbc264bdd4b372d45ce5f6c385caef247b
Ryan Schultz <i23586@verisk.com>
2023-10-04 06:42:26 -0600

    use prepare statements for users repository

--- database/user_repository.go
+++ database/user_repository.go
@@ -15,33 +15,57 @@ func NewUserRepository(db *sql.DB) *UserRepository {
 
 func (repo *UserRepository) GetUsersById(id int) (models.User, error) {
 	var user models.User
-	error := repo.db.QueryRow("SELECT * FROM Users WHERE userId=?", id).Scan(
-		&user.UserId, &user.Username, &user.Email, &user.FirstName, &user.LastName, &user.LastLogin,
-	)
+	query := "SELECT * FROM Users WHERE userId=?"
+
+	stmt, error := repo.db.Prepare(query)
 	if error != nil {
 		return user, error
+	}
+	defer stmt.Close()
+
+	err := stmt.QueryRow(id).Scan(
+		&user.UserId, &user.Username, &user.Email, &user.FirstName, &user.LastName, &user.LastLogin,
+	)
+	if err != nil {
+		return user, err
 	}
 	return user, nil
 }
 
 func (repo *UserRepository) GetUsersByName(name string) (models.User, error) {
 	var user models.User
-	error := repo.db.QueryRow("SELECT * FROM Users WHERE username=?", name).Scan(
+	query := "SELECT * FROM Users WHERE username=?"
+
+	stmt, error := repo.db.Prepare(query)
+	if error != nil {
+		return user,error
+	}
+	defer stmt.Close()
+
+	err := stmt.QueryRow(name).Scan(
 		&user.UserId, &user.Username, &user.Email, &user.FirstName, &user.LastName, &user.LastLogin,
 	)
-	if error != nil {
-		return user, error
+	if err != nil {
+		return user, err
 	}
 	return user, nil
 }
 
 func (repo *UserRepository) GetUserProfile(id int) (models.UserProfile, error) {
 	var user models.UserProfile
-	error := repo.db.QueryRow("SELECT * FROM Users WHERE userId=?", id).Scan(
-		&user.UserId, &user.Username, &user.Bio, &user.Signature, &user.FavCharId,
-	)
+	query := "SELECT * FROM Users WHERE userId=?"
+
+	stmt, error := repo.db.Prepare(query)
 	if error != nil {
 		return user, error
+	}
+	defer stmt.Close()
+
+	err := repo.db.QueryRow("SELECT * FROM Users WHERE userId=?", id).Scan(
+		&user.UserId, &user.Username, &user.Bio, &user.Signature, &user.FavCharId,
+	)
+	if err != nil {
+		return user, err
 	}
 	return user, nil
 }
